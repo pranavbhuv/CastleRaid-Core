be27a31612c9796cddb7e47257657e7c96d8b282
Blazing Games <iiflamiinblaze123@gmail.com>
2018-01-02 14:32:53 -0500

    Adding onJoin to BlazeListener :D

--- src/CRCore/Events/BlazeListener.php
+++ src/CRCore/Events/BlazeListener.php
@@ -17,10 +17,19 @@ use CRCore\Loader;
 
 # PocketMine uses:
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
 
 class BlazeListener implements Listener{
 
   public function __construct(Loader $main){
     $this->main = $main;
+  }
+  
+  public function onJoin(PlayerJoinEvent $event){
+    $player = $event->getPlayer();
+    $player->sendMessage(TextFormat::GOLD . "Blazes are love, Blazes are life");
+    $player->addTitle(TextFormat::GOLD . "Blazes are love, Blazes are life");
+    $player->sendPopup(TextFormat::GOLD . "Blazes are love, Blazes are life");
   }
 }
