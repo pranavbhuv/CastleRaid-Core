06fd545a16919647ac29e3db762195d7734d1b73
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2018-01-02 19:03:26 +0000

    No copy and paste totally

--- src/CRCore/Events/PotionListener.php
+++ src/CRCore/Events/PotionListener.php
@@ -1,1 +1,12 @@
-
+<?php
+/**
+ * -==+CastleRaid Core+==-
+ * Originally Created by QuiverlyRivarly
+ * Originally Created for CastleRaidPE
+ *
+ * @authors: QuiverlyRivarly and iiFlamiinBlaze
+ * @contributors: Nick, Potatoe, and Nice.
+ */
+namespace CRCore\PotionListener;
+# Loader use:
+use CRCore\Loader;
