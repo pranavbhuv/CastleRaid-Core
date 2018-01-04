370254f459dbd295ca5580efef8376e959c6585e
aki senpai | mommy gf <endercreeper4ut@gmail.com>
2018-01-04 17:48:31 -0500

    Update AlertTask.php

--- src/CRCore/Tasks/AlertTask.php
+++ src/CRCore/Tasks/AlertTask.php
@@ -1,1 +1,30 @@
-//TODO: AlertTask
+<?php
+/**
+ * -==+CastleRaid Core+==-
+ * Originally Created by QuiverlyRivarly
+ * Originally Created for CastleRaidPE
+ *
+ * @authors: QuiverlyRivarly and iiFlamiinBlaze
+ * @contributors: Nick, Potatoe, and Nice.
+ */
+declare(strict_types=1);
+
+namespace CRCore\Tasks;
+
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+
+class AlertTask extends PluginTask{
+
+    /** @var Loader $main */
+    public static $main;
+    private static $messages = array("Woah Cool Message", "Another Cool Message");
+  
+    public function onRun(){
+      index = array_rand($messages);
+      foreach(Server::getInstance()->getDefaultLevel()->getPlayers() as $player){
+        $player->sendMessage(TextFormat::RED . 'Alert' . TextFormat::GRAY . $messages[$index]);
+      }
+    }  
+}
