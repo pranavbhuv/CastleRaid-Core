536a848fb9c178ecd5288592a8703fda4e59c1a6
Blazing Games <iiflamiinblaze123@gmail.com>
2018-01-12 21:04:56 -0500

    Fixing a crashdump *ahem quiver ahem*

--- src/CRCore/Tasks/HealthBar.php
+++ src/CRCore/Tasks/HealthBar.php
@@ -2,18 +2,20 @@
 
 namespace Tasks\HealthBar;
 
-use pocketmine\{Server, Player};
+use pocketmine\Server;
+use pocketmine\Player;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Plugin;
 use pocketmine\utils\TextFormat;
 
 class HealthBar extends PluginTask {
+    
     public function __construct($plugin){
         $this->plugin = $plugin;
         parent::__construct($plugin);
     }
 
-    public function onRun($currentTick){
+    public function onRun(int $currentTick){
         foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p){
             $player = $p;
             $p->setNameTag(TextFormat::GREEN . $p->getName() . "§c[" . ($player->getHealth() / $player->getMaxHealth() * 20)."]");
