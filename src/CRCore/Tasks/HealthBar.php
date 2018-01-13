f5515ab259a11704c66c97a22587f685d9609947
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2018-01-13 01:32:51 +0000

    Update HealthBar.php

--- src/CRCore/Tasks/HealthBar.php
+++ src/CRCore/Tasks/HealthBar.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace Tasks\HealthBar;
+
+use pocketmine\{Server, Player};
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Plugin;
+use pocketmine\utils\TextFormat;
+
+class HealthBar extends PluginTask {
+    public function __construct($plugin){
+        $this->plugin = $plugin;
+        parent::__construct($plugin);
+    }
+
+    public function onRun($currentTick){
+        foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p){
+            $player = $p;
+            $p->setNameTag(TextFormat::GREEN . $p->getName() . "§c[" . ($player->getHealth() / $player->getMaxHealth() * 20)."]");
+        }
+    }
+}
