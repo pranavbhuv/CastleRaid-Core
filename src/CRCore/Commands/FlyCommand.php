9d007f2f968ecfed6cce462d7ac69a0c424b345b
NickTehUnicorn <ilhancana@gmail.com>
2018-01-14 14:04:18 +0300

    Fixed /fly
    Now it doesn't...
     - not work
     - turn your fly mode off when you're in creative

--- src/CRCore/Commands/FlyCommand.php
+++ src/CRCore/Commands/FlyCommand.php
@@ -4,7 +4,7 @@
  * Originally Created by QuiverlyRivarly
  * Originally Created for CastleRaidPE
  *
- * @authors: QuiverlyRivarly and iiFlamiinBlaze
+ * @authors     : QuiverlyRivarly and iiFlamiinBlaze
  * @contributors: Nick, Potatoe, and Nice.
  */
 declare(strict_types=1);
@@ -21,34 +21,39 @@ use pocketmine\utils\TextFormat;
  * Class FlyCommand
  * @package CRCore\Commands
  */
-class FlyCommand extends PluginCommand {
-    /**
-     * FlyCommand constructor.
-     * @param Loader $plugin
-     */
-    public function __construct(Loader $plugin) {
-        parent::__construct("fly", $plugin);
-        $this->setDescription("Allows you to fly in survival");
-        $this->setPermission("castleraid.fly");
-    }
+class FlyCommand extends PluginCommand{
 
-    /**
-     * @param CommandSender $sender
-     * @param string $commandLabel
-     * @param array $args
-     * @return bool|mixed|void
-     */
-    public function execute(CommandSender $sender, string $commandLabel, array $args) {
-        if ($this->testPermission($sender) and $sender instanceof Player) {
-            if (!$sender->getAllowFlight()) {
-                $sender->setAllowFlight(true);
-                $sender->sendMessage(TextFormat::GREEN . "Fly mode enabled.");
-            } else {
-                if ($sender->getAllowFlight()) {
-                    $sender->setAllowFlight(false);
-                    $sender->sendMessage(TextFormat::RED . "Fly mode disabled.");
-                }
-            }
-        }
-    }
+	/**
+	 * FlyCommand constructor.
+	 * @param Loader $plugin
+	 */
+	public function __construct(Loader $plugin){
+		parent::__construct("fly", $plugin);
+		$this->setDescription("Allows you to fly in survival");
+		$this->setPermission("castleraid.fly");
+	}
+
+	/**
+	 * @param CommandSender $sender
+	 * @param string        $commandLabel
+	 * @param array         $args
+	 * @return bool|mixed|void
+	 */
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+		if($this->testPermission($sender) and $sender instanceof Player){
+			if(!$sender->isCreative()){
+				if(!$sender->getAllowFlight()){
+					$sender->setAllowFlight(true);
+					$sender->sendMessage(TextFormat::GREEN . "Fly mode enabled.");
+				}else{
+					$sender->setAllowFlight(false);
+					$sender->setFlying(false);
+					$sender->sendMessage(TextFormat::RED . "Fly mode disabled.");
+				}
+			}else{
+				$sender->sendMessage(TextFormat::RED . "You are already in creative mode!");
+			}
+
+		}
+	}
 }
