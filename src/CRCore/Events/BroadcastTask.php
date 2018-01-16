2c562e9a044bffa4824f56822f70e0792de5c264
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2018-01-16 18:43:36 +0000

    Create BroadcastTask.php

---
+++ src/CRCore/Events/BroadcastTask.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * -==+CastleRaid Core+==-
+ * Originally Created by QuiverlyRivarly
+ * Originally Created for CastleRaidPE
+ *
+ * @authors: QuiverlyRivarly and iiFlamiinBlaze
+ * @contributors: Nick, Potatoe, and Nice.
+ */
+declare(strict_types=1);
+
+namespace CRCore\Events;
+
+use CRCore\Loader;
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+
+class BroadcastTask extends PluginTask {
+
+    private $main;
+
+    public function __construct(Loader $main) {
+        $this->main = $main;
+    }
+
+    public function onRun(int $currentTick) {
+        $input = [
+            "Message 1",
+		    	  "Message 2",
+            "Message 3"
+        ];
+		   $details = array_rand($input);
+		   Server::getInstance()->broadcastMessage($input[$details]);
+    }
+}
