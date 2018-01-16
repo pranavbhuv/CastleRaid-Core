ab997ff04c5318a000f1663894f1548130f07ef0
Blazing Games <iiflamiinblaze123@gmail.com>
2018-01-16 12:24:56 -0500

    Stage 1: Starting Quest Development
    * Starting Quest Command Development

--- src/CRCore/Commands/QuestsCommand.php
+++ src/CRCore/Commands/QuestsCommand.php
@@ -16,6 +16,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
+use jojoe77777\FormAPI;
 
 class QuestsCommand extends PluginCommand {
 
@@ -27,7 +28,32 @@ class QuestsCommand extends PluginCommand {
 
     public function execute(CommandSender $sender, string $commandLabel, array $args) {
         if ($this->testPermission($sender) and $sender instanceof Player) {
-            $sender->sendMessage(TextFormat::RED . "Coming Soon");
+            $this->mainForm($sender);
         }
+    }
+
+    public function mainForm(Player $player) {
+        $api = $this->getPlugin()->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $form = $api->createSimpleForm(function (Player $sender, array $data) {
+            if (isset($data[0])) {
+                switch ($data[0]) {
+                    case 0:
+                        $sender->sendMessage(TextFormat::RED . "Exiting QuestUI...");
+                        break;
+                    case 1:
+                        $sender->sendMessage(TextFormat::RED . "Coming Soon!");
+                        break;
+                    case 2:
+                        $sender->sendMessage(TextFormat::RED . "Coming Soon!");
+                        break;
+                }
+            }
+        });
+        $form->setTitle("Quest UI");
+        $form->setContent(TextFormat::AQUA . "Tap any of the available quests below!");
+        $form->addButton(TextFormat::DARK_RED . "Exit");
+        $form->addButton(TextFormat::GREEN . "Quest | Knights");
+        $form->addButton(TextFormat::GREEN . "Quest | Raiding");
+        $form->sendToPlayer($player);
     }
 }
