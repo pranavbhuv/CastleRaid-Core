15ab6eaa3a009670350ceb667e4cbb48068dab83
NickTehUnicorn <ilhancana@gmail.com>
2018-01-21 13:25:21 +0300

    I don't even know what I'm doing

--- src/CRCore/API.php
+++ src/CRCore/API.php
@@ -27,6 +27,7 @@ class API {
     const NO_PERMISSION = TextFormat::BOLD . TextFormat::GRAY . "(" . TextFormat::RED . "!" . TextFormat::GRAY . ")" . TextFormat::RED . "You don't have permission to use this command";
     const NOT_PLAYER = TextFormat::BOLD . TextFormat::GRAY . "(" . TextFormat::RED . "!" . TextFormat::GRAY . ")" . TextFormat::RED . "Use this command in-game";
     const CORE_VERSION = "v1.4.6";
+    const QUEST_PREFIX = TextFormat::GREEN . "Quests " . TextFormat::AQUA . "> ";
 
     public static function getRandomName() : string{
         $n = self::$names->getNested("names");
