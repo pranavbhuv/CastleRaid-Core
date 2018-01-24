ffcf9700dba79bb3ea6b9718893ae722c95906a1
Blazing Games <iiflamiinblaze123@gmail.com>
2018-01-24 11:13:34 -0500

    Implementing Hud Task
    * Adding repeatingtask to loader
    * Creating HudTask

---
+++ src/CRCore/Tasks/HudTask.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * -==+CastleRaid Core+==-
+ * Originally Created by QuiverlyRivarly
+ * Originally Created for CastleRaidPE
+ *
+ * @authors: QuiverlyRivarly and iiFlamiinBlaze
+ * @contributors: Nick, Potatoe, and Jason.
+ */
+declare(strict_types=1);
+
+namespace CRCore\Tasks;
+
+use CRCore\API;
+use CRCore\Loader;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+
+class HudTask extends PluginTask{
+
+    private $plugin;
+
+    public function __construct(Loader $owner){
+        parent::__construct($owner);
+        $this->plugin = $owner;
+    }
+
+    public function onRun(int $currentTick){
+        foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+            $player->sendTip(TextFormat::GRAY . "-==+" . TextFormat::GREEN . "CastleRaid" . TextFormat::GRAY . "+==-" . PHP_EOL); //Edit this with the message you want for the hud Quiver
+        }
+    }
+}
