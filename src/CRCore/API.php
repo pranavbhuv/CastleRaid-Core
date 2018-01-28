3f1e73508411f9ec109eea6d4b492e8c1d763641
Angel <bubblicious712@gmail.com>
2018-01-28 12:53:18 -0500

    Update API.php

--- src/CRCore/API.php
+++ src/CRCore/API.php
@@ -28,7 +28,6 @@ class API{
     public static $msg;
     
     const NOT_PLAYER = TextFormat::BOLD . TextFormat::GRAY . "(" . TextFormat::RED . "!" . TextFormat::GRAY . ")" . TextFormat::RED . "Use this command in-game!";
-    const CORE_VERSION = "v1.4.6";
     const QUEST_PREFIX = TextFormat::GREEN . "Quests " . TextFormat::AQUA . "> " . TextFormat::WHITE;
     const FEEDBACK_PREFIX = TextFormat::BLUE . "Feedback" . "> " . TextFormat::WHITE;
 
