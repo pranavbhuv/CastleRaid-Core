62b662cd46e7adb9dd729a0832753a1be5383037
NickTehUnicorn <ilhancana@gmail.com>
2018-01-28 13:09:37 +0300

    Bleh

---
+++ src/CRCore/Commands/MailCommand.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilhan
+ * Date: 24.01.2018
+ * Time: 09:20
+ */
+
+namespace CRCore\Commands;
+
+
+class MailCommand{
+
+}
---
+++ src/CRCore/Forms/MailForm.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilhan
+ * Date: 24.01.2018
+ * Time: 09:22
+ */
+
+namespace CRCore\Forms;
+
+
+class MailForm{
+
+}
