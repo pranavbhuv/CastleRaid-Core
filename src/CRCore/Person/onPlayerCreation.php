5aa120eba94f9a80f2700756a3c91392f4cf4f89
Angel <bubblicious712@gmail.com>
2018-01-28 12:35:12 -0500

    added constructor to register event

--- src/CRCore/Person/onPlayerCreation.php
+++ src/CRCore/Person/onPlayerCreation.php
@@ -7,7 +7,11 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerCreationEvent;
 
 class onPlayerCreation implements Listener{
-
+    
+    public function __construct(Loader $core){
+        $core->getServer()->getPluginManager()->registerEvents($this, $core);
+    }
+    
     public function onCreation(PlayerCreationEvent $creationEvent){
         if ($creationEvent->getPlayerClass() == Person::class) return;
 
