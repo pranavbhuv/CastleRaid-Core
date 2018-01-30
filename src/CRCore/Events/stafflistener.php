4660b76bd9bb5f212cd6d773d00ba8bee3774f83
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2018-01-30 10:18:06 +0000

    Create stafflistener.php

---
+++ src/CRCore/Events/stafflistener.php
@@ -0,0 +1,9 @@
+
+
+/**
+ * When player types staff help, it will ping staff
+ * When the command /hacker (name) is run, it will ping staff on a hacker
+ * When the command /blacklist is run, it will deop everyone, and op only a specified list, as well as whitelisting the server.
+ * When staff does /staff is run, a helpful staffui is run, for staff to use
+ * When staff does /stafflist is run it sends player list of staff
+ */
