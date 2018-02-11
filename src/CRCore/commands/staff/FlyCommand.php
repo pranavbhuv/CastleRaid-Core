e0afe059352bfb884a6e7f773c18dfa828175cb2
Angel <24781244+Teamblocket@users.noreply.github.com>
2018-02-11 14:06:15 -0500

    Update FlyCommand.php

--- src/CRCore/commands/staff/FlyCommand.php
+++ src/CRCore/commands/staff/FlyCommand.php
@@ -10,7 +10,6 @@ declare(strict_types=1);
 
 namespace CRCore\commands\staff;
 
-use CRCore\API;
 use CRCore\commands\BaseCommand;
 use CRCore\Loader;
 use pocketmine\command\CommandSender;
@@ -25,7 +24,7 @@ class FlyCommand extends BaseCommand{
 
     public function execute(CommandSender $sender, string $commandLabel, array $args){
         if(!$sender instanceof Player){
-            $sender->sendMessage(API::NOT_PLAYER);
+            $sender->sendMessage('Only In-Game');
             return false;
         }
         if(!$sender->hasPermission("castleraid.fly")){
